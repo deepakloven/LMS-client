6f77ca6487ff291b8031af2b85cf3aa9c8acd570
DwashKarma <dwaskc@gmail.com>
2024-07-31 08:55:25 +0545

    feat: navabar initialized

--- components/nav-bar/navbar.tsx
+++ components/nav-bar/navbar.tsx
@@ -1,4 +1,10 @@
 const NavBarComponent = () => {
-  return <div className="p-5">Navbar</div>;
+  return (
+    <div className="p-5 bg-slate-200 flex justify-around">
+      <div>Logo</div>
+      <div className="w-64">Search</div>
+      <div>Avatar</div>
+    </div>
+  );
 };
 export default NavBarComponent;
